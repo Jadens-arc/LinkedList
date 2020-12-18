fa2faca273526b1a8d75ce684e9c210de93296dc
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-12-17 16:46:52 -0800

    added remove function

--- LinkedList.js
+++ LinkedList.js
@@ -7,6 +7,20 @@ function LinkedList() {
 	this.head = new Node();
 	this.length = 0;
 
+	this.remove = index => {
+		let prevNode = null;
+		let curNode = this.head;
+		let curIndex = -1;
+
+		while (curIndex != index) {
+			prevNode = curNode;
+			curNode = curNode.next;
+			curIndex ++;
+		}
+
+		prevNode.next = curNode.next;
+		
+	}
 
 	/**
 	 * Append an item to the list
@@ -40,7 +54,7 @@ myList.push(5);
 myList.push(6);
 myList.push(7);
 myList.push(125);
-
+myList.remove(0);
 myList.display();
 
 
