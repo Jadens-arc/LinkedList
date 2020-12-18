e3a93b7b9544b53b14752daa6f672dabdc36a005
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-12-17 16:38:24 -0800

    added linked list with display and push

--- LinkedList.js
+++ LinkedList.js
@@ -3,3 +3,44 @@ function Node(value=null) {
 	this.next = null;
 }
 
+function LinkedList() {
+	this.head = new Node();
+	this.length = 0;
+
+
+	/**
+	 * Append an item to the list
+	 * @param {*} data 
+	 */
+	this.push = value => {
+		let curNode = this.head;
+		while (curNode.next) {
+			curNode = curNode.next;
+		}
+		curNode.next = new Node(value);
+	}
+
+	/**
+	 * Display all item in list
+	 */
+	this.display = () => {
+		let curNode = this.head;
+		while (curNode.next) {
+			curNode = curNode.next;
+			console.log(curNode.value);
+		}
+	}
+}
+
+
+let myList = new LinkedList();
+myList.push(1);
+myList.push(3);
+myList.push(5);
+myList.push(6);
+myList.push(7);
+myList.push(125);
+
+myList.display();
+
+
